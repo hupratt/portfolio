b506d987c142000f9fd6f0ff76773c97f82ce8c2
hupratt <hp@hp.com>
2019-05-24 19:42:24 +0200

    test new repo

--- src/Portfolio/settings.py
+++ src/Portfolio/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 from django.utils.translation import ugettext_lazy as _
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)  
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
