a7d2d19b6bf1145635ae00032d54a579576f4f21
hupratt <hprattdo@gmail.com>
2020-01-03 20:35:18 +0100

    nice pop up effect

---
+++ src/posts/static/js/popup.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+		$('.js-fh5co-nav-toggle').on('click', function(event) {
+			event.preventDefault();
+			if( $('body').hasClass('menu-show') ) {
+				$('body').removeClass('menu-show');
+				$('#fh5co-main-nav > .js-fh5co-nav-toggle').removeClass('show');
+			} else {
+				$('body').addClass('menu-show');
+				setTimeout(function(){
+					$('#fh5co-main-nav > .js-fh5co-nav-toggle').addClass('show');
+				}, 900);
+            }
+        });
+    });
+
+	    
