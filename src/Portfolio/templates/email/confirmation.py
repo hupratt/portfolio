3d11561c2d398d1b8aea076674c6b51b19fa542c
hupratt <hprattdo@gmail.com>
2020-01-05 18:55:14 +0100

    black text on the template

--- src/Portfolio/templates/email/confirmation.py
+++ src/Portfolio/templates/email/confirmation.py
@@ -94,16 +94,16 @@ template="""
 <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 5px; padding-bottom: 30px; font-family: Arial, sans-serif"><![endif]-->
 <div style="color:#555555;font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;line-height:1.5;padding-top:5px;padding-right:10px;padding-bottom:30px;padding-left:10px;">
 <div style="font-size: 12px; line-height: 1.5; color: #555555; font-family: Cabin, Arial, Helvetica Neue, Helvetica, sans-serif; mso-line-height-alt: 18px;">
-<p style="font-size: 14px; line-height: 1.5; word-break: break-word; mso-line-height-alt: 21px; margin: 0;">Thank you for your email. Here is a copy of the message you've sent us.</p>
-<p style="font-size: 14px; line-height: 1.5; word-break: break-word; mso-line-height-alt: 21px; margin: 0;"><br/> message = {}</p>
-<p style="font-size: 14px; line-height: 1.5; word-break: break-word; mso-line-height-alt: 21px; margin: 0;">subject = {}<br/> urgency = {}<br/> pricerange = {}<br/><br/></p>
+<p style="font-size: 14px; line-height: 1.5; color:black; word-break: break-word; mso-line-height-alt: 21px; margin: 0;">Thank you for your email. Here is a copy of the message you've sent us.</p>
+<p style="font-size: 14px; line-height: 1.5; color:black; word-break: break-word; mso-line-height-alt: 21px; margin: 0;"><br/> message = {}</p>
+<p style="font-size: 14px; line-height: 1.5; color:black; word-break: break-word; mso-line-height-alt: 21px; margin: 0;">subject = {}<br/> urgency = {}<br/> pricerange = {}<br/><br/></p>
 </div>
 </div>
 <!--[if mso]></td></tr></table><![endif]-->
 <!--[if mso]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding-right: 10px; padding-left: 10px; padding-top: 20px; padding-bottom: 10px; font-family: Arial, sans-serif"><![endif]-->
 <div style="color:#555555;font-family:Cabin, Arial, Helvetica Neue, Helvetica, sans-serif;line-height:1.2;padding-top:20px;padding-right:10px;padding-bottom:10px;padding-left:10px;">
 <div style="font-size: 12px; line-height: 1.2; color: #555555; font-family: Cabin, Arial, Helvetica Neue, Helvetica, sans-serif; mso-line-height-alt: 14px;">
-<p style="font-size: 16px; line-height: 1.2; word-break: break-word; mso-line-height-alt: 19px; margin: 0;"><span style="font-size: 16px;">Thanks so much for contacting <a href="https://portfolio.pythonbots.site/">craft studios</a> ! </span></p>
+<p style="color:black; font-size: 16px; line-height: 1.2; word-break: break-word; mso-line-height-alt: 19px; margin: 0;"><span style="font-size: 16px;">Thanks so much for contacting <a href="https://portfolio.pythonbots.site/">craft studios</a> ! </span></p>
 </div>
 </div>
 <!--[if mso]></td></tr></table><![endif]-->
