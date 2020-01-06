04d54982d5a2860f2b351ca0aeac57d72dac8d29
hupratt <hprattdo@gmail.com>
2020-01-06 21:34:27 +0100

    resolving bug where form was not doing the validation thing

--- src/posts/static/js/validator.init.js
+++ src/posts/static/js/validator.init.js
@@ -1,17 +1,25 @@
-// initialize a validator instance from the "FormValidator" constructor.
-// A "<form>" element is optionally passed as an argument, but is not a must
-var validator = new FormValidator({ events: ["blur"] }, document.forms[0]);
-validator.settings.classes.alert = "alert alert-warning";
-// on form "submit" event
-document.forms[0].onsubmit = function(e) {
-  var submit = true,
-    validatorResult = validator.checkAll(this);
 
-  console.log(validatorResult);
-  return !!validatorResult.valid;
-};
+window.addEventListener("load", () => {
 
-// on form "reset" event
-document.forms[0].onreset = function(e) {
-  validator.reset();
-};
+var formsCollection = document.getElementsByTagName("form");
+for(var i=0;i<formsCollection.length;i++){
+    // initialize a validator instance from the "FormValidator" constructor.
+    // A "<form>" element is optionally passed as an argument, but is not a must
+    var validator = new FormValidator({ events: ["blur"] }, formsCollection[i]);
+    validator.settings.classes.alert = "alert alert-warning";
+    // on form "submit" event
+    formsCollection[i].onsubmit = function(e) {
+      var submit = true,
+        validatorResult = validator.checkAll(this);
+    
+      console.log(validatorResult);
+      return !!validatorResult.valid;
+    };
+    
+    // on form "reset" event
+    formsCollection[i].onreset = function(e) {
+      validator.reset();
+    };
+  }
+
+});
