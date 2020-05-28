9b47beda577a8185cfec656919b1fb586f5d6660
hupratt <hprattdo@gmail.com>
2020-05-28 19:48:10 +0200

    adding sentry stuff

--- src/Portfolio/settings.py
+++ src/Portfolio/settings.py
@@ -208,10 +208,9 @@ SENDGRID_API_KEY = os.environ.get("SENDGRID_API_KEY")
 if os.environ.get("DJANGO_DEVELOPMENT") is None:
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
-    # SENTRY_KEY = os.getenv("SENTRY_KEY_port","")
     sentry_sdk.init(
         dsn="https://"
-        + SENTRY_KEY
+        + os.getenv("SENTRY_KEY","")
         + "@sentry.io/1890369",  
         integrations=[DjangoIntegration()],
 
