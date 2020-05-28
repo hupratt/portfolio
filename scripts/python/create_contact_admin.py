b66536907aca49a4f183d9cd4304532ed13a417a
hupratt <hprattdo@gmail.com>
2020-05-28 11:30:41 +0200

    improving the chat

--- scripts/python/create_contact_admin.py
+++ scripts/python/create_contact_admin.py
@@ -1,6 +1,25 @@
-from chat.models import Contact
+from chat.models import Contact, Chat, Message
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 admin, _ = Contact.objects.get_or_create(user=User.objects.get(pk=1))
 admin.image_url = 'https://avatars2.githubusercontent.com/u/38809698?s=460&u=a10478aa9a5a6709c32eec29f016150d8f5bd242&v=4'
 admin.save()
+
+guest, _ = Contact.objects.get_or_create(user=User.objects.get(pk=2))
+
+new = User.objects.create(username='new')
+new.save()
+newcontact, _ = Contact.objects.get_or_create(user=User.objects.get(pk=3))
+
+
+conversation, _ = Chat.objects.get_or_create(participants=guest)
+
+conversation.participants.add(newcontact)
+conversation.save()
+conversation.participants.all()
+
+message = Message.objects.create(contact= admin,content=f"Hello {guest.user.first_name}, send me a message and i'll get back to you ASAP")
+conversation.messages.add(message)
+conversation.save()
+
--- src/chat/signals.py
+++ src/chat/signals.py
@@ -1,7 +1,7 @@
 from allauth.account.signals import user_signed_up
 from django.dispatch import receiver
 from django.core.files import File
-from chat.models import Contact
+from chat.models import Contact, Chat, Message
 import urllib, logging
 
 logger = logging.getLogger("django")
@@ -25,10 +25,21 @@ def user_signed_up_(request, user, sociallogin=None, **kwargs):
         logger.info(f'name: {name}')
         logger.info(f'sociallogin.account: {sociallogin.account}')
         picture_path = sociallogin.account.get_avatar_url()
-        contact_instance, _ = Contact.objects.get_or_create(user=user, image_url=picture_path)
+        guest, _ = Contact.objects.get_or_create(user=user, image_url=picture_path)
         admin, _ = Contact.objects.get_or_create(user=1)
         if isinstance(admin, Contact):
-            contact_instance.friends.add(admin)
-            contact_instance.save()
+            guest.friends.add(admin)
+            guest.save()
+
+            # open up a conversation with the admin
+            conversation, _ = Chat.objects.get_or_create(participants=guest)
+            conversation.participants.add(admin)
+            conversation.save()
+            
+            # strike up a conversation with the guest
+            message = Message.objects.create(contact= admin,content=f"Hello {guest.user.first_name}, drop me a message here and I'll get back to you as soon as possible")
+            conversation.messages.add(message)
+            conversation.save()
+
         else:
             raise ValueError("Admin's contact could not be found")
