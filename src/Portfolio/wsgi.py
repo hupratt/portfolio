d4cfc8fd08a00b73abb38691217ff8390350dcf9
hupratt <hprattdo@gmail.com>
2020-05-28 20:01:01 +0200

    asyncio re appeared and wsgi wrong again

--- src/Portfolio/wsgi.py
+++ src/Portfolio/wsgi.py
@@ -7,17 +7,31 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os, sys, dotenv
+import os, sys, dotenv, logging
+
+logger = logging.getLogger("django")
 
 from django.core.wsgi import get_wsgi_application
 
 if os.environ.get("DJANGO_DEVELOPMENT") == "true":
-    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)),'.env.development'))
+    dotenv.read_dotenv(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
+            ".env.development",
+        )
+    )
 else:
-    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.abspath(__file__)),'.env'))
+    logger.info(
+        f'grabbing env file in path: {os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env")}'
+    )
+    dotenv.read_dotenv(
+        os.path.join(
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".env"
+        )
+    )
 
 sys.path.append("/home/ubuntu/Dev/PythonBots")
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Portfolio.settings")
 
 application = get_wsgi_application()
