655ba61c8f5911457ff12a25c34a720ccf1d77f1
hupratt <hprattdo@gmail.com>
2020-07-22 18:50:22 +0200

    🐛 Fixing the emails not sending

--- src/Portfolio/views.py
+++ src/Portfolio/views.py
@@ -53,70 +53,58 @@ def index(request):
     today = timezone.now().date()
     form = EmailPostForm(request.POST or None)
     if form.is_valid() and request.method == "POST":
-        # Form fields passed validation
-        """ Begin reCAPTCHA validation """
-        recaptcha_response = request.POST.get("g-recaptcha-response")
-        data = {
-            "secret": settings.GOOGLE_RECAPTCHA_SECRET_KEY,
-            "response": recaptcha_response,
-        }
-        r = requests.post("https://www.google.com/recaptcha/api/siteverify", data=data)
-        result = r.json()
-        """ End reCAPTCHA validation """
 
-        if result["success"]:
-            cd = form.cleaned_data
-            from sendgrid import SendGridAPIClient
-            from sendgrid.helpers.mail import Mail, HtmlContent
+        cd = form.cleaned_data
+        from sendgrid import SendGridAPIClient
+        from sendgrid.helpers.mail import Mail, HtmlContent
 
-            message = Mail(
-                from_email=cd["email"],
-                to_emails=settings.EMAIL_HOST_RECIPIENT,
-                subject=cd["subject"],
-                html_content=HtmlContent(
-                    template.format(
-                        str(cd["firstname"]) + " " + str(cd["lastname"]),
-                        str(cd["message"]),
-                        str(cd["subject"]),
-                        str(cd["urgency"]),
-                        str(cd["pricerange"]),
-                    )
-                ),
-            )
-            try:
+        message = Mail(
+            from_email=cd["email"],
+            to_emails=settings.EMAIL_HOST_RECIPIENT,
+            subject=cd["subject"],
+            html_content=HtmlContent(
+                template.format(
+                    str(cd["firstname"]) + " " + str(cd["lastname"]),
+                    str(cd["message"]),
+                    str(cd["subject"]),
+                    str(cd["urgency"]),
+                    str(cd["pricerange"]),
+                )
+            ),
+        )
+        try:
+            sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+            response = sg.send(message)
+            if cd["sendcc"] is True:
+                from sendgrid import SendGridAPIClient
+                from sendgrid.helpers.mail import Mail, HtmlContent
+
                 sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
-                response = sg.send(message)
-                if cd["sendcc"] is True:
-                    from sendgrid import SendGridAPIClient
-                    from sendgrid.helpers.mail import Mail, HtmlContent
+                message2 = Mail(
+                    from_email="noreply@craft.studios",
+                    to_emails=cd["email"],
+                    subject=cd["subject"],
+                    html_content=HtmlContent(
+                        template.format(
+                            str(cd["firstname"]) + " " + str(cd["lastname"]),
+                            str(cd["message"]),
+                            str(cd["subject"]),
+                            str(cd["urgency"]),
+                            str(cd["pricerange"]),
+                        )
+                    ),
+                )
 
-                    sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
-                    message2 = Mail(
-                        from_email="noreply@craft.studios",
-                        to_emails=cd["email"],
-                        subject=cd["subject"],
-                        html_content=HtmlContent(
-                            template.format(
-                                str(cd["firstname"]) + " " + str(cd["lastname"]),
-                                str(cd["message"]),
-                                str(cd["subject"]),
-                                str(cd["urgency"]),
-                                str(cd["pricerange"]),
-                            )
-                        ),
-                    )
-
-                    response = sg.send(message2)
-            except Exception as e:
-                messages.error(request, f"Your message could not be sent. {e}")
-                return HttpResponseRedirect("/")
-
-            messages.success(
-                request,
-                "Your message was successfully sent to: "
-                + settings.EMAIL_HOST_RECIPIENT,
-            )
+                response = sg.send(message2)
+        except Exception as e:
+            messages.error(request, f"Your message could not be sent. {e}")
             return HttpResponseRedirect("/")
+
+        messages.success(
+            request,
+            "Your message was successfully sent to: " + settings.EMAIL_HOST_RECIPIENT,
+        )
+        return HttpResponseRedirect("/")
     else:
         form = EmailPostForm()
     return render(request, "index.html", {"today": today, "form": form})
